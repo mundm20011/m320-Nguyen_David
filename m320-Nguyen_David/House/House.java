1497c8021abe45ef2ae0cf5b010cab32298b0094
mundm20011 <burgerdavid6@gmail.com>
2024-08-23 09:35:15 +0200

    first commit

---
+++ m320-Nguyen_David/House/House.java
@@ -0,0 +1,30 @@
+package House;
+// Pan / Class of the house
+public class House {
+
+    // Attrubutes of the house
+    private String familyName;
+    private String street;
+    private int houseNumber;
+    private int numberOfRooms;
+    private double price;
+
+    // initializes a new house-object
+    public House() {
+        familyName = ""; // we dont know yet, we set to an initial value
+        street = "";
+        houseNumber = 0;
+        numberOfRooms = 0;
+        price = 0.0;
+    }
+
+    // getter and setter here
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+}
